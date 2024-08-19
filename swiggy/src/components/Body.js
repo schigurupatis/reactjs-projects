e7178f3b8fb26bdda81729609d1632110bea7def
schigurupatis <schigurupatis@gmail.com>
2024-08-19 14:15:34 +0530

    Fetching Data from Swiggy API and Rendering UI

--- swiggy/src/components/Body.js
+++ swiggy/src/components/Body.js
@@ -1,12 +1,24 @@
 import RestaurantCard from "./RestaurantCard"
 import resList from "../utils/mockData"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 
 const Body = () => {
   
     const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+    useEffect(() => {
+        //console.log("useEffect Called")
+        fetchData();
+    }, [])
     
+    const fetchData = async () => {
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=14.44840&lng=79.98880&page_type=DESKTOP_WEB_LISTING')
+        const json = await data.json();
+        //console.log(json)
+        setListOfRestaurants(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+    }
+
     return (
         <div className="body">
             <div className="container p-20">
