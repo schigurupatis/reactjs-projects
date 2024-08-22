44afadb63c04cbd35bfe6b756120386984ef885a
schigurupatis <schigurupatis@gmail.com>
2024-08-22 16:00:39 +0530

    setting count and updating it both in class & functional components

--- swiggy/src/components/User.js
+++ swiggy/src/components/User.js
@@ -1,7 +1,18 @@
+import { useState } from "react"
+
 const User = ({name, designation, location, contact}) => {
+
+    const [count1, setCount1] = useState(1);
+    const [count2, setCount2] = useState(2);
+
     return(
         <div className="user-card">
             <h1>Functional Component</h1>
+            <br />
+            <h1>Count1 is: {count1}</h1>
+            <br />
+            <h1>Count2 is: {count2}</h1>
+            <br />
             <h3>Name: {name}</h3>
             <h4>Designatio: {designation}</h4>
             <h2>Location: {location}</h2>
--- swiggy/src/components/UserClass.js
+++ swiggy/src/components/UserClass.js
@@ -4,12 +4,23 @@ class UserClass extends React.Component {
     constructor(props) {
         super(props)
         //console.log(props)
+
+        this.state = {
+            count1: 1,
+            count2: 2,
+        }
     }
+
     render () {
         const {name, designation, location, contact} = this.props;
         return (
             <div className="user-card">
                 <h1>Classbased Component</h1>
+                <br />
+                    <h1>Count1 is: {this.state.count1}</h1>
+                <br />
+                    <h1>Count2 is: {this.state.count2}</h1>
+                <br />
                 <h3>Name: {name}</h3>
                 <h4>Designatio: {designation}</h4>
                 <h2>Location: {location}</h2>
