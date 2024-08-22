de229db5b525743145c3a264774f2eee8502caa1
schigurupatis <schigurupatis@gmail.com>
2024-08-22 08:40:25 +0530

    working with functional component pros and destructing on the fly

--- swiggy/src/components/User.js
+++ swiggy/src/components/User.js
@@ -1,11 +1,11 @@
-const User = () => {
+const User = ({name, designation, location, contact}) => {
     return(
         <div className="user-card">
             <h1>Functional Component</h1>
-            <h3>Name: Santha Kumar Chigurupati</h3>
-            <h4>Designatio: UI/UX Engineer</h4>
-            <h2>Location: Hyderabad</h2>
-            <h3>Contact: @schigurupatis</h3>
+            <h3>Name: {name}</h3>
+            <h4>Designatio: {designation}</h4>
+            <h2>Location: {location}</h2>
+            <h3>Contact: {contact}</h3>
         </div>
     )
 }
