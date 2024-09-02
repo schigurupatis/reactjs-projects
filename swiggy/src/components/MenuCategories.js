994e6ef33a7dbc1905b22810e6bcc3a36013b963
schigurupatis <schigurupatis@gmail.com>
2024-09-02 22:29:57 +0530

    adding restaurant menu categories

---
+++ swiggy/src/components/MenuCategories.js
@@ -0,0 +1,17 @@
+const MenuCategories = (data) => {
+    //console.log(data)
+    return (
+        <div>
+            <div className="accordion">
+                <div className="accordion-header">
+                    <h1>test</h1>
+                </div>
+                <div className="accordion-body">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, minus?</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MenuCategories
--- swiggy/src/components/RestaurantMenu.js
+++ swiggy/src/components/RestaurantMenu.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import Shimmer from "./Shimmer"
 import { useParams } from "react-router-dom"
 import useRestaurantMenu from "../utils/useRestaurantMenu"
+import MenuCategories from "./MenuCategories"
 
 const RestaurantMenu = () => {
 
@@ -76,10 +77,10 @@ const RestaurantMenu = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="menu-cards">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <div className="menu-categories">
+                        {
+                            categories.map((category) => <MenuCategories data={category} />)
+                        }
                     </div>
                 </div>
             </div>
