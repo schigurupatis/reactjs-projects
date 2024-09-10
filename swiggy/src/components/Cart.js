a9c63edaa81102357b8439fd8c1354872fbbf9bd
schigurupatis <schigurupatis@gmail.com>
2024-09-10 23:58:05 +0530

    clear cart added with payload removeItem

--- swiggy/src/components/Cart.js
+++ swiggy/src/components/Cart.js
@@ -1,11 +1,18 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ItemList from "./ItemList";
+import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
-  console.log(cartItems);
+  //console.log(cartItems);
+
+  const dispatch = useDispatch();
+  const handleClear = () => {
+    //console.log("clear clicked");
+    dispatch(clearCart());
+  };
 
   return (
     <div className="bg-slate-100">
@@ -16,6 +23,14 @@ const Cart = () => {
             <span className="text-orange-600">{cartItems.length}</span> items
           </h1>
           <ItemList items={cartItems} />
+          <div>
+            <button
+              className="btn bg-black text-white rounded px-5 py-3"
+              onClick={handleClear}
+            >
+              Clear Cart
+            </button>
+          </div>
         </div>
       </div>
     </div>
