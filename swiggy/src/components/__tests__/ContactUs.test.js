4066b7fdc6740765f90edbf0707918404d3ca7ca
schigurupatis <schigurupatis@gmail.com>
2024-09-12 12:34:11 +0530

    excluded coverage folder in gitignore

--- swiggy/src/components/__tests__/ContactUs.test.js
+++ swiggy/src/components/__tests__/ContactUs.test.js
@@ -2,51 +2,54 @@ import ContactUs from "../ContactUs";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-test("should load contactus component", () => {
-  //Rendering
-  render(<ContactUs />);
+// Grouping Test-Cases
+describe("Contactus page test-cases", () => {
+  test("should load contactus component", () => {
+    //Rendering
+    render(<ContactUs />);
 
-  //Quering
-  const headingEl = screen.getByRole("heading");
+    //Quering
+    const headingEl = screen.getByRole("heading");
 
-  //Asserting
-  expect(headingEl).toBeInTheDocument();
-});
+    //Asserting
+    expect(headingEl).toBeInTheDocument();
+  });
 
-test("should load button in contactus component", () => {
-  //Rendering
-  render(<ContactUs />);
+  test("should load button in contactus component", () => {
+    //Rendering
+    render(<ContactUs />);
 
-  //Quering
-  const buttonEl = screen.getByRole("button");
+    //Quering
+    const buttonEl = screen.getByRole("button");
 
-  //Assering
-  // Asserting
-  expect(buttonEl).toBeInTheDocument();
-  expect(buttonEl.textContent).toBe("Send Message");
-});
+    //Assering
+    // Asserting
+    expect(buttonEl).toBeInTheDocument();
+    expect(buttonEl.textContent).toBe("Send Message");
+  });
 
-test("should load input name inside contactus component", () => {
-  //Rendering
-  render(<ContactUs />);
+  test("should load input name inside contactus component", () => {
+    //Rendering
+    render(<ContactUs />);
 
-  //Quering
-  const inputName = screen.getByLabelText("Name");
+    //Quering
+    const inputName = screen.getByLabelText("Name");
 
-  //Asserting
-  expect(inputName).toBeInTheDocument();
-  //expect(inputName).toHaveTextContent("Enter Name");
-});
+    //Asserting
+    expect(inputName).toBeInTheDocument();
+    //expect(inputName).toHaveTextContent("Enter Name");
+  });
 
-test("should load 4 input boxes on the contactus component", () => {
-  //Rendering
-  render(<ContactUs />);
+  test("should load 4 input boxes on the contactus component", () => {
+    //Rendering
+    render(<ContactUs />);
 
-  //Quering
-  const inputBoxes = screen.getAllByRole("textbox");
+    //Quering
+    const inputBoxes = screen.getAllByRole("textbox");
 
-  //console.log(inputBoxes.length);
+    //console.log(inputBoxes.length);
 
-  //Asserting
-  expect(inputBoxes.length).toBe(4);
+    //Asserting
+    expect(inputBoxes.length).toBe(4);
+  });
 });
