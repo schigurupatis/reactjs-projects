f782bae18ce103a15b7756e7e32fd2f714b5024a
schigurupatis <schigurupatis@gmail.com>
2024-09-12 11:56:24 +0530

    written test cases for label, inputs and buttons

--- swiggy/src/components/__tests__/ContactUs.test.js
+++ swiggy/src/components/__tests__/ContactUs.test.js
@@ -3,15 +3,43 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 test("should load contactus component", () => {
+  //Rendering
   render(<ContactUs />);
 
+  //Quering
   const headingEl = screen.getByRole("heading");
 
   //Asserting
   expect(headingEl).toBeInTheDocument();
 });
 
-test("should load 2 input boxes on the contactus component", () => {
+test("should load button in contactus component", () => {
+  //Rendering
+  render(<ContactUs />);
+
+  //Quering
+  const buttonEl = screen.getByRole("button");
+
+  //Assering
+  // Asserting
+  expect(buttonEl).toBeInTheDocument();
+  expect(buttonEl.textContent).toBe("Send Message");
+});
+
+test("should load input name inside contactus component", () => {
+  //Rendering
+  render(<ContactUs />);
+
+  //Quering
+  const inputName = screen.getByLabelText("Name");
+
+  //Asserting
+  expect(inputName).toBeInTheDocument();
+  //expect(inputName).toHaveTextContent("Enter Name");
+});
+
+test("should load 4 input boxes on the contactus component", () => {
+  //Rendering
   render(<ContactUs />);
 
   //Quering
@@ -20,5 +48,5 @@ test("should load 2 input boxes on the contactus component", () => {
   //console.log(inputBoxes.length);
 
   //Asserting
-  expect(inputBoxes.length).toBe(5);
+  expect(inputBoxes.length).toBe(4);
 });
