2861c37819fd45a6c3a75019fc371dee7718a9b0
schigurupatis <schigurupatis@gmail.com>
2024-09-12 11:09:56 +0530

    working  with different types of test-cases like loading component, finding elements like inputs, buttons...etc in that component

--- swiggy/src/components/__tests__/ContactUs.test.js
+++ swiggy/src/components/__tests__/ContactUs.test.js
@@ -7,5 +7,18 @@ test("should load contactus component", () => {
 
   const headingEl = screen.getByRole("heading");
 
+  //Asserting
   expect(headingEl).toBeInTheDocument();
+});
+
+test("should load 2 input boxes on the contactus component", () => {
+  render(<ContactUs />);
+
+  //Quering
+  const inputBoxes = screen.getAllByRole("textbox");
+
+  //console.log(inputBoxes.length);
+
+  //Asserting
+  expect(inputBoxes.length).toBe(5);
 });
