f085d0b13b91bf43bc3fb5add64943a229eb1b4d
schigurupatis <schigurupatis@gmail.com>
2024-09-19 10:50:31 +0530

    youtube project created and font setup

---
+++ youtube/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h3 className="text-lg font-bold text-red-600">YouTube App Goes Here</h3>
+    </div>
+  );
+}
+
+export default App;
