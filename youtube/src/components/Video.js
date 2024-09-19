a18a90cbd398b695ad6ae9d3a7ad1e85c109e7f0
schigurupatis <schigurupatis@gmail.com>
2024-09-19 19:49:04 +0530

    working with youtube fetch api data

--- youtube/src/components/Video.js
+++ youtube/src/components/Video.js
@@ -24,12 +24,13 @@ function Video() {
         <div className="w-1/12">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            enable-background="new 0 0 24 24"
+            enableBackground="new 0 0 24 24"
             height="24"
             viewBox="0 0 24 24"
             width="24"
             focusable="false"
             aria-hidden="true"
+            className="cursor-pointer"
           >
             <path d="M12 16.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5-1.5-.67-1.5-1.5.67-1.5 1.5-1.5zM10.5 12c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5-1.5.67-1.5 1.5zm0-6c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5-1.5.67-1.5 1.5z"></path>
           </svg>
--- youtube/src/components/VideoContainer.js
+++ youtube/src/components/VideoContainer.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Video from "./Video";
+import { YOUTUBE_VIDEOS_API } from "../utils/constants";
 
 function VideoContainer() {
+  useEffect(() => {
+    getVideosData();
+  }, []);
+
+  const getVideosData = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await data.json();
+    console.log(json);
+  };
+
   return (
     <div className="flex flex-wrap justify-between items-center">
       <Video />
