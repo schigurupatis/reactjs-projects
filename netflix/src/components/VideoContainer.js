2388edc432627398ca9e746a7a89d59c3568867c
Santha Kumar Chigurupati <santhakumarchigurupati@Santhas-MacBook-Air.local>
2024-10-16 22:56:05 +0530

    VideoTitle & VideoBackground components created with VideoContainer Main Movie

--- netflix/src/components/VideoContainer.js
+++ netflix/src/components/VideoContainer.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import VideoBackground from "./VideoBackground";
+import VideoTitle from "./VideoTitle";
 
 const VideoContainer = () => {
-  return <div>VideoContainer</div>;
+
+  const movies = useSelector(store => store.movies?.nowPlayingMovies)
+
+  const mainMovie = movies[0];
+  console.log("Main Movie is: ", mainMovie)
+
+  return (
+  <div>
+    <VideoTitle />
+    <VideoBackground />
+  </div>
+  );
 };
 
 export default VideoContainer;
