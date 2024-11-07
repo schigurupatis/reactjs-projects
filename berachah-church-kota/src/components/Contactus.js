ad93409812e3734bbc4e814a08a674e0582ce99a
Santha Kumar Chigurupati <schigurupatis@gmail.com>
2024-11-07 17:15:13 +0530

    files setup

--- berachah-church-kota/src/components/Contactus.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Contactus = () => {
-  return <div>Contactus</div>;
-};
-
-export default Contactus;
--- berachah-church-kota/src/utils/constants.js
+++
@@ -1,2 +0,0 @@
-export const bannerimg1 = "../../public/img/banner1.jpg";
-export const bannerimg2 = "./img/banner1.jpg";
